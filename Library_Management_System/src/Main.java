bb89d77cc9fc7f51bd08a5b1bfb84467114723b2
LeanRin <64357374+LeanRin@users.noreply.github.com>
2025-05-24 00:08:45 +0800

    Update Main.java

--- Library_Management_System/src/Main.java
+++ Library_Management_System/src/Main.java
@@ -2,8 +2,5 @@ public class Main {
     public static void main(String[] args) {
     //载入登录窗口
     new LoginUI();
-        new MenutSystemUI("admin", "管理员");
-        new MenutSystemUI("user", "普通用户");
-
     }
 }
